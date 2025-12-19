173c3648a95c797c4536ddbc9e6ebf3755310f1b
hieuwin10 <g.hieu12345@gmail.com>
2025-12-19 14:24:30 +0700

    Checkpoint from VS Code for cloud agent session

--- obj/Debug/net9.0-windows/DoAnChamSocSucKhoe.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/DoAnChamSocSucKhoe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnChamSocSucKhoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a17dfe36736ae2d8f58fcf2126edead5c11e30")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnChamSocSucKhoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnChamSocSucKhoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
